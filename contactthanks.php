bdd1a38a53dbb0d105c69c694a144afc477e9687
Roastbeast <65375250+Roastbeast@users.noreply.github.com>
2022-11-06 09:53:05 -0500

    Initializing Repository

---
+++ contactthanks.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Thank You!</title> 
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+	<div id="page-wrap">
+		
+			
+		<h1>Thanks! </h1>
+        
+        
+       <p>  Your message has been sent!</h1><br />
+		
+		<p><a href="index.html">Back to muskokaframing.ca</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
